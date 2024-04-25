bfa7c137c3ce41a3f5f68a162244b199a66c19a1
agioldas <aleksis.gioldaseas@outlook.com>
2024-04-26 00:26:16 +0300

    2017 Day 2 Solution

--- 2017-D1/Part1/main.go
+++ 2017-D1/Part1/main.go
@@ -10,16 +10,16 @@ var input string = `ENTER_YOUR_INPUT_HERE`
 func main() {
 	// number := demo[0]
 	number := input
-	counter := 0
+	P1Answer := 0
 	for i := 0; i < len(number)-1; i++ { //interate over each position on the string
 		if number[i] == number[i+1] { //if char is the same as the next char
-			counter += int(number[i] - '0') //turning char to int
+			P1Answer += int(number[i] - '0') //turning char to int
 		}
 	}
 	if number[0] == number[len(number)-1] { //checking if first matches the last one since the sequence is a circle
-		counter += int(number[0] - '0')
+		P1Answer += int(number[0] - '0')
 	}
 
-	fmt.Println("Part 1:", counter)
+	fmt.Println("Part 1:", P1Answer)
 
 }
