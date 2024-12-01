7b713b655a21b40e8b96c55664fd4b0742c5644a
agioldas <aleksis.gioldaseas@outlook.com>
2024-12-01 13:39:51 +0200

    2024 Day 1 Part 1 & 2

--- 2017-D0/Part1/main.go
+++ 2017-D0/Part1/main.go
@@ -1,22 +1,7 @@
 package main
 
-import "strings"
-
 var input string = `YOUR_INPUT_HERE`
 
 func main() {
-
-	for _, line := range strings.Split(input, "\n") {
-		if line == "" {
-			continue
-		}
-
-		for _, thing := range strings.Split(line, ",") {
-			if thing == "" {
-				continue
-			}
-
-		}
-	}
 
 }
