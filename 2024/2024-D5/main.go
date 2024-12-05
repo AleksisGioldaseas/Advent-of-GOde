ce79fe5b00e7760904683128db26b54bb336b81c
Aleksis <aleksis.gioldaseas@outlook.com>
2024-12-05 17:13:07 +0200

    2024 Day 5 Part 1 & 2

--- 2024/2024-D5/main.go
+++ 2024/2024-D5/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
 
 type page struct {
-	before []int
-	after  []int
+	left []int
+	right  []int
 }
 
 func main() {
@@ -19,57 +20,86 @@ func main() {
 	fmt.Println("Part 2 answer:", part2(input))
 }
 
+var rulesMap = make(map[int]page)
+
 func part1(input string) string {
-	theMap := make(map[int]page)
 	parts := strings.Split(input, "\n\n")
 
-	pages := strings.Split(parts[1], "\n")
+	books := strings.Split(parts[1], "\n")
 	orderRules := strings.Split(parts[0], "\n")
 
-	for _, rule := range orderRules {
-		parts := strings.Split(rule, "|")
-
-		left := atoi(parts[0])
-		right := atoi(parts[1])
-
-		val, ok := theMap[left]
+	initMap(orderRules)
+	
+	total := 0
+	for _, pages := range books {
+		slice, ok := pagesAreSorted(pages)
 		if ok {
-			val.after = append(val.after, right)
-			theMap[left] = val
-		} else {
-			theMap[left] = page{after: []int{right}}
-		}
-
-		val, ok = theMap[right]
-		if ok {
-			val.before = append(val.before, left)
-			theMap[right] = val
-		} else {
-			theMap[right] = page{after: []int{left}}
-		}
-	}
-
-	for _, line := range pages {
-		for _, pageVal := range strings.Split(line, ",") {
-
-			fmt.Println(pageVal, theMap[atoi(pageVal)])
+			total += slice[len(slice)/2]
 		}
 	}
 
 	// inputLines := strings.Split(input, "\n")
-	part1Answer := ""
-	return part1Answer
 
+	return fmt.Sprint(total)
 }
 
 func part2(input string) string {
+	parts := strings.Split(input, "\n\n")
+
+	books := strings.Split(parts[1], "\n")
+	
+
+	total := 0
+	for _, pages := range books {
+		slice, ok := pagesAreSorted(pages)
+		if !ok {
+			slice = sortSlice(slice)
+			total += slice[len(slice)/2]
+			// fmt.Println(slice, ok, slice[len(slice)/2])
+		}
+		
+	}
+
 	// inputLines := strings.Split(input, "\n")
-	part2Answer := ""
-	return part2Answer
+
+	return fmt.Sprint(total)
+}
+
+func printMap(){
+	for k, v := range rulesMap{
+		fmt.Println(k,v)
+	}
+	fmt.Println()
+}
+
+func initMap(orderRules []string) {
+	for _, rule := range orderRules {
+		// fmt.Println(rule)
+		parts := strings.Split(rule, "|")
+
+		A := atoi(parts[0])
+		B := atoi(parts[1])
+
+		val, ok := rulesMap[A]
+		if ok {
+			val.right = append(val.right, B)
+			rulesMap[A] = val
+		} else {
+			rulesMap[A] = page{right: []int{B}}
+		}
+
+		val, ok = rulesMap[B]
+		if ok {
+			val.left = append(val.left, A)
+			rulesMap[B] = val
+		} else {
+			rulesMap[B] = page{left: []int{A}}
+		}
+		// printMap()
+	}
 }
 
 func atoi(str string) int {
-
 	val, err := strconv.Atoi(str)
 	if err != nil {
 		fmt.Println("yo, bad numbers", str, err)
@@ -78,8 +108,80 @@ func atoi(str string) int {
 	return val
 }
 
+func pagesAreSorted(pages string) ([]int, bool) {
+	pageNums := []int{}
+	for _, pageStr := range strings.Split(pages, ",") {
+		pageNums = append(pageNums, atoi(pageStr))
+	}
+	for start, num := range pageNums {
+		if start == len(pageNums)-1 {
+			continue
+		}
+		Lpage := rulesMap[num]
+		for i2 := start + 1; i2 < len(pageNums); i2++ {
+			if slices.Contains(Lpage.left, pageNums[i2]) {
+				return pageNums, false
+			}
+		}
+	}
+	return pageNums, true
+}
+
+func sortSlice(slice []int) []int {
+	// fmt.Println("Sort this", slice)
+	newSlice := []int{slice[0]}
+	slice = slice[1:]
+	for _, addMe := range slice {
+		// fmt.Println("so far: ",newSlice)
+		stillNeedToAdd := true
+		// fmt.Println("first\n")
+		for i := 0; i < len(newSlice); i++ {
+			
+			// fmt.Println(i, "addme:",addMe, newSlice)
+			// fmt.Println("sides",rulesMap[addMe])
+
+			if slices.Contains(rulesMap[addMe].right, newSlice[i]){
+				// fmt.Println("insert ",addMe)
+				newSlice = slices.Insert(newSlice, i, addMe)
+				stillNeedToAdd = false
+				break
+			}			
+		}
+		// fmt.Println("second\n")
+		if stillNeedToAdd{
+			for i := len(newSlice)-1; i >= 0; i-- {
+				
+				// fmt.Println(i, "addme:",addMe, newSlice)
+				// fmt.Println("sides",rulesMap[addMe])
+
+				if slices.Contains(rulesMap[addMe].left, newSlice[i]){
+					// fmt.Println("insert ",addMe)
+					newSlice = slices.Insert(newSlice, i+1, addMe)
+					stillNeedToAdd = false
+					break
+				}
+			}
+		}
+		// if stillNeedToAdd{
+		// 	for i := len(newSlice)-1; i >= 0; i-- {
+		// 		if slices.Contains(rulesMap[newSlice[i]].left, addMe){
+		// 			newSlice = slices.Insert(newSlice, i+1, addMe)
+		// 			stillNeedToAdd = false
+		// 			break
+		// 		}
+		// 	}
+		// }
+		if stillNeedToAdd{
+			// fmt.Println("append")
+			newSlice = append(newSlice, addMe)
+		}
+	}
+	// fmt.Println("---------------------final: ",newSlice)
+	return newSlice
+}
+
 func getInput() string {
-	fileName := "demo.txt"
+	fileName := "input.txt"
 	data, err := os.ReadFile(fileName)
 	if err != nil {
 		fmt.Println("Error opening file")
@@ -89,6 +191,6 @@ func getInput() string {
 		fmt.Println(fileName, " file is empty")
 		os.Exit(1)
 	}
-	input := strings.ReplaceAll(string(data), "\r\n", "\n") //doing this replace so it can handle both linux and window text format
-	return strings.TrimSpace(input)                         //doing this cause usually there's an extra new line at the bottom of the input
+	input := strings.ReplaceAll(string(data), "\r\n", "\n") // doing this replace so it can handle both linux and window text format
+	return strings.TrimSpace(input)                         // doing this cause usually there's an extra new line at the bottom of the input
 }
